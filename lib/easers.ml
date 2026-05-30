7710e9a2a521274ba9625f2c0729405ea5f75f7c
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-05-29 19:08:02 -0700

    Added more easers.

--- lib/easers.ml
+++ lib/easers.ml
@@ -7,6 +7,16 @@ let expo x = match x with
   | 0.0 -> 0.0
   | _   -> 2.0 ** (10.0 *. x -. 10.0)
 let circ x = 1.0 -. (1.0 -. x ** 2.0) ** 0.5
+let back x =
+  let c1 = 1.70158 in
+  let c2 = c1 +. 1.0 in
+  c2 *. (x ** 3.0) -. c1 *. (x ** 2.0)
+let elastic (x : float) : float =
+  let c = (2.0 *. Float.pi) /. 3.0 in
+  match x with
+  | 0.0 -> 0.0
+  | 1.0 -> 1.0
+  | _   -> Float.neg (2.0 ** (10.0 *. x -. 10.0)) *. Float.sin((x *. 10.0 -. 10.75) *. c)
 let bounce x = 
   let n = 7.5625 in
   let d = 2.5 in
