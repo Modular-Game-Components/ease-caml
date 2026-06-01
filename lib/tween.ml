4916c852ef84035779fd766fe1547fe11d59ca2f
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-06-01 12:27:12 -0700

    Refactored to use binary tree representation for tweens.

--- lib/tween.ml
+++ lib/tween.ml
@@ -4,21 +4,15 @@ type tween_node =
   end_val: float;
   ease_func: float -> float;
   mutable progress: float;
+  repeat: int;
+  mutable cur_repeat: int;
   obj: float ref;
 }
 
-type flat_tween =
-{
-  mutable index : int;
-  repeat : int;
-  mutable cur_repeat : int;
-  tweens: tween_node array;
-}
-
-type tween = Flat of flat_tween 
+type tween = Node of tween_node
            | Nested of {
-             tweens: tween array;
-             mutable index: int;
+             tween_left: tween;
+             tween_right: tween;
              repeat: int;
              mutable cur_repeat : int
            } 
@@ -31,77 +25,75 @@ let make_tween_node (sv: float) (ev: float) (ef: float -> float) (obj: float ref
   progress = 0.0;
   ease_func = ef;
   obj = obj;
+  repeat = 1;
+  cur_repeat = 0;
 }
 
 let make_tween (sv: float) (ev: float) (ef: float -> float) (obj: float ref) : tween =
   let tween_node = make_tween_node sv ev ef obj in 
-  Flat {
-    index = 0;
-    repeat = 1;
-    cur_repeat = 0;
-    tweens = [|tween_node|];
-  }
+  Node tween_node
 
 let repeat (t: tween) (count: int) = match t with
-  | Flat t -> Flat {index = 0; repeat = count; cur_repeat = 0; tweens = t.tweens }
-  | Nested t -> Nested {index = 0; repeat = count; cur_repeat = 0; tweens = t.tweens}
+  | Node t -> Node {
+                repeat = count;
+                cur_repeat = 0;
+                start_val = t.start_val;
+                end_val = t.end_val;
+                ease_func = t.ease_func;
+                progress = 0.0;
+                obj = t.obj;
+              }
+  | Nested t -> Nested {
+                  repeat = count;
+                  cur_repeat = 0;
+                  tween_left = t.tween_left;
+                  tween_right = t.tween_right;
+                }
+
+let should_restart_node (tn: tween_node) = 
+        tn.progress >= 1.0 && (tn.cur_repeat < tn.repeat - 1 || tn.repeat = -1)
+let node_finished (tn: tween_node) = tn.progress >= 1.0 
+                                  && tn.cur_repeat = tn.repeat - 1
 
 let update_node (node: tween_node) (dt: float) : unit =
-  let p = node.ease_func (node.progress +. dt) in
-  let sv = node.start_val in
-  let ev = node.end_val in
-  node.progress <- node.progress +. dt;
-  node.obj := (1.0 -. p) *. sv +. p *. ev
+  match should_restart_node node with
+    | true -> node.progress <- 0.0; 
+              node.cur_repeat <- node.cur_repeat + 1;
+              node.obj := node.start_val
+    | false ->  let p = node.ease_func (node.progress +. dt) in
+                let sv = node.start_val in
+                let ev = node.end_val in 
+                node.progress <- node.progress +. dt;
+                node.obj := (1.0 -. p) *. sv +. p *. ev
 
-let node_finished (tn: tween_node) = tn.progress >= 1.0
-
-let reset_node (tn: tween_node) : unit = tn.progress <- 0.0
 let rec reset_tween (t: tween) = match t with
-  | Flat t -> t.cur_repeat <- 0; t.index <- 0; Array.iter reset_node t.tweens
-  | Nested t -> t.cur_repeat <- 0; t.index <- 0; Array.iter reset_tween t.tweens
+  | Node t -> t.cur_repeat <- 0; 
+              t.progress <- 0.0;
+  | Nested t -> t.cur_repeat <- 0;
+                reset_tween t.tween_left;
+                reset_tween t.tween_right
 
-let should_restart_flat (t: flat_tween) : bool = 
-    t.index = Array.length t.tweens - 1 
- && node_finished t.tweens.(t.index) 
- && (t.cur_repeat < t.repeat - 1 || t.repeat = ~-1)
-
-let flat_tween_finished (t: flat_tween) =
-    t.index = Array.length t.tweens - 1
- && node_finished t.tweens.(t.index)
- && not (should_restart_flat t)
-
-let rec should_restart (t: tween) : bool = match t with
-  | Flat t -> should_restart_flat t
-  | Nested t -> (t.cur_repeat < t.repeat || t.repeat = ~-1)
-              && t.index >= Array.length t.tweens - 1
-              && is_finished t.tweens.(t.index)
-and is_finished (t: tween) : bool = match t with
-  | Flat t -> flat_tween_finished t
+let rec is_finished (t: tween) : bool = match t with
+  | Node t -> node_finished t 
   | Nested t -> (t.cur_repeat = t.repeat && t.repeat <> ~-1) 
-              && t.index >= Array.length t.tweens - 1 
-              && is_finished t.tweens.(t.index)
-
-let rec update_flat_tween (t: flat_tween) (dt: float) : unit =
-  if not (node_finished t.tweens.(t.index)) then update_node t.tweens.(t.index) dt
-  else if t.index < Array.length t.tweens - 1 then begin
-    t.index <- t.index + 1;
-    update_flat_tween t dt
-  end
-  else if should_restart_flat t then begin
-    t.index <- 0;
-    t.cur_repeat <- t.cur_repeat + 1;
-    Array.iter reset_node t.tweens
-  end
+              && is_finished t.tween_right
 
 let rec update_tween (t: tween) (dt: float) : unit = match t with
-  | Flat t -> update_flat_tween t dt
-  | Nested t -> if not (is_finished t.tweens.(t.index)) then update_tween t.tweens.(t.index) dt
-  else if should_restart (Nested t) then begin 
-    t.index <- 0;
-    Array.iter reset_tween t.tweens;
-    t.cur_repeat <- t.cur_repeat + 1
-  end
+  | Node t -> update_node t dt
+  | Nested t -> if not (is_finished t.tween_left) then
+      update_tween t.tween_left dt
+    else if not (is_finished t.tween_right) then
+      update_tween t.tween_right dt
 
+let extend (t1: tween) (t2: tween) =
+  Nested {
+    tween_left = t1;
+    tween_right = t2;
+    repeat = 1;
+    cur_repeat = 0;
+  }
+
+let ( $> ) = extend
 
 let update (tm: tween_manager) (dt: float) : unit =
   List.iter (fun x -> update_tween x dt) !tm;
