d5b9125cbd00bd74e29182f8a10da6f18001bae7
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-05-31 20:18:35 -0700

    Repeat example now working.

--- lib/tween.ml
+++ lib/tween.ml
@@ -25,9 +25,6 @@ type tween = Flat of flat_tween
              mutable cur_repeat : int
            } 
 
-let node_done (tn: tween_node) =
-  if tn.progress > 1.0 then true else false
-
 type tween_manager = tween list ref
 
 let make_tween_node (sv: float) (ev: float) (ef: float -> float) (obj: float ref) = {
@@ -45,7 +42,7 @@ let make_tween (sv: float) (ev: float) (ef: float -> float) (obj: float ref) : t
   Flat {
     index = 0;
     repeat = 1;
-    cur_repeat = 1;
+    cur_repeat = 0;
     tweens = [|tween_node|];
   }
 
@@ -64,57 +61,55 @@ let node_finished (tn: tween_node) = tn.progress >= 1.0
 
 let reset_node (tn: tween_node) : unit = tn.progress <- 0.0
 let rec reset_tween (t: tween) = match t with
-  | Flat t -> t.cur_repeat <- 0;
-  | Nested t -> Array.iter reset_tween t.tweens
+  | Flat t -> t.cur_repeat <- 0; t.index <- 0; Array.iter reset_node t.tweens
+  | Nested t -> t.cur_repeat <- 0; t.index <- 0; Array.iter reset_tween t.tweens
 
 let should_restart_flat (t: flat_tween) : bool = 
-  if t.index < Array.length t.tweens - 1 then false
-  else node_finished t.tweens.(t.index) || t.cur_repeat < t.repeat
+    t.index = Array.length t.tweens - 1 
+ && node_finished t.tweens.(t.index) 
+ && (t.cur_repeat < t.repeat - 1 || t.repeat = ~-1)
 
 let flat_tween_finished (t: flat_tween) =
-  if t.index < Array.length t.tweens - 1 then false
-  else should_restart_flat t
+    t.index = Array.length t.tweens - 1
+ && node_finished t.tweens.(t.index)
+ && not (should_restart_flat t)
 
 let rec should_restart (t: tween) : bool = match t with
   | Flat t -> should_restart_flat t
-  | Nested t -> t.cur_repeat < t.repeat && is_finished t.tweens.(t.index)
+  | Nested t -> (t.cur_repeat < t.repeat || t.repeat = ~-1)
+              && t.index >= Array.length t.tweens - 1
+              && is_finished t.tweens.(t.index)
 and is_finished (t: tween) : bool = match t with
   | Flat t -> flat_tween_finished t
-  | Nested t -> t.repeat = 0 && t.index = Array.length t.tweens && is_finished t.tweens.(t.index)
-
-let rec tween_finished (t: tween) = match t with
-  | Flat t -> flat_tween_finished t
-  | Nested t -> if tween_finished t.tweens.(t.index) 
-    && t.index = Array.length t.tweens
-    && not (should_restart (Nested t)) then true else false
+  | Nested t -> (t.cur_repeat = t.repeat && t.repeat <> ~-1) 
+              && t.index >= Array.length t.tweens - 1 
+              && is_finished t.tweens.(t.index)
 
 let rec update_flat_tween (t: flat_tween) (dt: float) : unit =
   if not (node_finished t.tweens.(t.index)) then update_node t.tweens.(t.index) dt
-  else if t.index < Array.length t.tweens - 1 then t.index <- t.index + 1
+  else if t.index < Array.length t.tweens - 1 then begin
+    t.index <- t.index + 1;
+    update_flat_tween t dt
+  end
   else if should_restart_flat t then begin
     t.index <- 0;
     t.cur_repeat <- t.cur_repeat + 1;
     Array.iter reset_node t.tweens
   end
-  else begin
-    Array.iter reset_node t.tweens;
-    t.index <- t.index + 1
-  end
 
 let rec update_tween (t: tween) (dt: float) : unit = match t with
   | Flat t -> update_flat_tween t dt
-  | Nested t -> if t.index < Array.length t.tweens then update_tween t.tweens.(t.index) dt
+  | Nested t -> if not (is_finished t.tweens.(t.index)) then update_tween t.tweens.(t.index) dt
   else if should_restart (Nested t) then begin 
     t.index <- 0;
     Array.iter reset_tween t.tweens;
-    t.cur_repeat <- t.cur_repeat + 1;
-    update_tween t.tweens.(0) dt
+    t.cur_repeat <- t.cur_repeat + 1
   end
 
 
 let update (tm: tween_manager) (dt: float) : unit =
   List.iter (fun x -> update_tween x dt) !tm;
-  tm := List.filter (fun x -> not (tween_finished x)) !tm
+  tm := List.filter (fun x -> not (is_finished x)) !tm
 
 let new_manager () : tween_manager = ref []
 
--- lib/tween.mli
+++ lib/tween.mli
@@ -4,7 +4,10 @@ type tween
 type tween_manager = tween list ref
 
 val make_tween : float -> float -> (float -> float) -> float ref -> tween
+val repeat : tween -> int -> tween
+
 val update_tween : tween -> float -> unit
 val update : tween_manager -> float -> unit
+
 val new_manager : unit -> tween_manager
 val add : tween -> tween_manager -> unit
